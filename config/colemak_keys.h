f6408c4cfdce43dc3e7cbdb8317dddee4a284135
kerbyfc <kerbyfc@gmail.com>
2026-05-13 10:36:16 +0300

    Create colemak_keys.h

---
+++ config/colemak_keys.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+
+#define CM_COLON (LS(ZMK_HID_USAGE(HID_USAGE_KEY, HID_USAGE_KEY_KEYBOARD_P)))
